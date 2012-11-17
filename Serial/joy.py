a41ec2fbc53cc6af51c5c35107c53cc600aa5c31
JohnLZeller <johnlzeller@gmail.com>
2012-11-16 20:43:47 -0800

    Updated joy.py to newly agreed upon controller configuration, added 'Y' as software mode switch and changed vector values to 0-255

--- Serial/joy.py
+++ Serial/joy.py
@@ -6,6 +6,9 @@
 # Listener is listening to what Joy places into this queue and will package
 # and send the appropriate information to the receptionist
 
+### Left to Implement ###################################################
+# RJ - Vector Steer Mode - translate_joy				#
+
 ### Abilities ###########################################################
 # LJ - Controls all left wheels forward/reverse (no left/right) 	#
 # RJ - Controls all right wheels forward/reverse (no left/right)	#
@@ -38,6 +41,11 @@
 # 2) In case of artifacts in serial communication, initialize communication with all 0	#
 #    values for speeds, just in case the rover is given a GO command, when it shouldn't	#
 
+
+# Y - Software Change ( Skid / Vector )
+# LJ - Left wheels
+# RJ - Right Wheels
+
 import sys
 import threading
 from roverpacket import *
@@ -52,30 +60,26 @@ class JoyParser(object):
 		self.joy_queue = queue
 		# Creates lists and dictionaries
 		self.templist = []
-		self.joy_254 = {}
-		self.joy_127 = {}
-		self.joymap_counter_254 = float(0.00)
-		self.joymap_counter_127 = float(0.00)
 		self.left_wheel_ids = [2, 3, 4] # Front to Back
 		self.right_wheel_ids = [5, 6, 7] # Front to Back
 		self.tripod_id = 8
 		self.arm_id = 9
+		# Tracks the press of 'Y' which changes control from Skid to Vector
+		self.control_change = 0
 		self.buttons = {'\x00':'A', '\x01':'B', '\x02':'X', '\x03':'Y', \
                                 '\x04':'LB', '\x05':'RB', '\x06':'Back', '\x07':'Start', \
                                 '\x08':'Middle', '\t':'LJ/Button', '\n':'RJ/Button'}
-		# Initializes lists and dictionaries
+		# State dictionary to track previous values for all gamepad actions
+		self.states = {'A':0, 'B':0, 'X':0, 'Y':0, 'LB':0, 'RB':0,  \
+				'Back':0, 'Start':0, 'LJ/Button':0, 'RJ/Button':0, \
+				'Middle':0, 'Left':0, 'Right':0, 'Up':0, 'Down':0, \
+				'LJ/Left':0, 'LJ/Right':0, 'LJ/Up':0, 'LJ/Down':0, \
+				'RJ/Left':0, 'RJ/Right':0, 'RJ/Up':0, 'RJ/Down':0}
+		# Initializes templist
 		for x in range(8):
 		        self.templist.append(0)
-		for x in range(0,255):
-		        self.joy_254[x] = float(self.joymap_counter_254)
-		        self.joymap_counter_254 += float(0.390625)
-		for x in range(0,128):
-		        self.joy_127[x] = float(self.joymap_counter_127)
-		        self.joymap_counter_127 += float(0.7874015748)
 
 	def run(self):
-		# Reads from bus.joy_rover
-		# Parses info coming from there
 		# Figures out the info for 6 packets each
 		# Info needed: <addr> <motor_addr> <speed>
 		# SEND BogiePacket 6 packets each time, it expects the previous info
@@ -88,7 +92,7 @@ class JoyParser(object):
 		while 1:
 			# Returns list
 			# for buttons,
-			# for joy, list of 6 tuples, each holding <addr 2-7> <motor_addr> <speed> <angle>
+			# for joy, list of 6 tuples, each holding <addr 2-7> <speed> <angle>
 			pressed_button = 0
 			pressed_joy = 0
 			released_button = 0
@@ -104,7 +108,14 @@ class JoyParser(object):
 				found = 1
 				pressed_button = self.parse_pressed_button() # Grab the return here
 				if pressed_button is not None:
-					info_list = translate_button(pressed_button)
+					if pressed_button[0]=='Y' and self.control_change==0:
+						# Control Scheme for Vector
+						self.control_change = 1
+					elif pressed_button[0]=='Y' and self.control_change==1:
+						# Control Scheme for Skid
+						self.control_change = 0
+					else:
+						info_list = translate_button(pressed_button)
 				# If else, do nothing
 				
 			# JOYSTICK is PRESSED
@@ -129,7 +140,7 @@ class JoyParser(object):
 	
 	def translate_button(self, button_data):
 		# Returns list
-		# for wheels, 6 lists, each holding <addr 2-7> <motor_addr> <speed>
+		# for wheels, 6 lists, each holding <addr 2-7> <speed> <angle>
 		# for else, 
 		name = button_data[0]
 		data = button_data[1]
@@ -137,162 +148,135 @@ class JoyParser(object):
 		
 		# Tripod Controls
 		if name=='Left':
-			thing = 0 # Do
+			info_list = None
 		elif name=='Right':
-			thing = 0 # Do
+			info_list = None
 		elif name=='Up':
-			thing = 0 # Do
+			info_list = None
 		elif name=='Down':
-			thing = 0 # Do
+			info_list = None
 		
 		# Does something
 		elif name=='A':
-			thing = 0 # Do
+			info_list = None
 		elif name=='B':
-			thing = 0 # Do
+			info_list = None
 		elif name=='X':
-			thing = 0 # Do
-		elif name=='Y':
-			thing = 0 # Do
+			info_list = None
+		# elif name=='Y': <-- Checked in def run already
 			
-		# Actuation Controls - Example: <addr 2-7> <2> <speed>
-		# Assuming that Reverse is Left and Forward is Right
 		elif name=='LB':
-			# All Motor 2's Reverse at 5% speed
-			for wheel_id in self.left_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 2, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 2, speed
-					info_list.append(data_tuple)
-			for wheel_id in self.right_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 2, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 2, speed
-					info_list.append(data_tuple)
+			info_list = None
 		elif name=='RB':
-			# All Motor 2's Forward at 5% speed
-			for wheel_id in self.left_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 2, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data # makes forward # redundant, but human readable
-					data_tuple = wheel_id, 2, speed
-					info_list.append(data_tuple)
-			for wheel_id in self.right_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 2, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data # makes forward # redundant, but human readable
-					data_tuple = wheel_id, 2, speed
-					info_list.append(data_tuple)
+			info_list = None
 			
 		# Does Something
 		elif name=='Back':
-			thing = 0 # Do
+			info_list = None
 		elif name=='Start':
-			thing = 0 # Do
+			info_list = None
 		elif name=='Middle': # Emergency Stop EVERYTHING?
-			thing = 0 # Do
+			info_list = None
 		elif name=='LJ/Button':
-			thing = 0 # Do
+			info_list = None
 		elif name=='RJ/Button':
-			thing = 0 # Do
+			info_list = None
+			
+		return info_list
+		#			info_list.append(data_tuple)
 		
 	def translate_joy(self, joy_data):
-		# Returns list of 6 lists, each holding <addr 2-7> <motor_addr> <speed>
-		# NOTE: Speed is always rounded down to nearest number
+		# Returns list of 6 lists, each holding <addr 2-7> <speed> <angle>
+		# NOTE: Speed is for drive, angle is for actuation
 		name = joy_data[0]
-		data = int(joy_data[1]) # Rounded down
+		data = joy_data[1]
 		info_list = []
 		
-		# LT and RT just skid steer in place - Example: <addr 2-7> <1> <speed>
-		if name=='LT':
-			# Left wheels Reverse, Right wheels Forward
-			for wheel_id in self.left_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
+		if self.control_change==0: # Skid Steer Mode
+			# LT and RT just skid steer in place - Example: <addr 2-7> <speed> <0>
+			if name=='LT':
+				# Left wheels Reverse, Right wheels Forward
+				for wheel_id in self.left_wheel_ids:
+					if data==0:
+						data_tuple = wheel_id, 0, 0
+						info_list.append(data_tuple)
+					else:	# Reverse (0-127)
+						data = int(data / 2) # Round down
+						data_tuple = wheel_id, data, 0
+						info_list.append(data_tuple)
+				for wheel_id in self.right_wheel_ids:
+					if data==0:
+						data_tuple = wheel_id, 0, 0
+						info_list.append(data_tuple)
+					else:	# Forward (255-128)
+						data = int(data / 2) + 128 # Round down
+						data_tuple = wheel_id, data, 0
+						info_list.append(data_tuple)
+			elif name=='RT':
+				# Left wheels Forward, Right wheels Reverse
+				for wheel_id in self.left_wheel_ids:
+					if data==0:
+						data_tuple = wheel_id, 0, 0
+						info_list.append(data_tuple)
+					else:	# Reverse (0-127)
+						data = int(data / 2) + 128 # Round down
+						data_tuple = wheel_id, data, 0
+						info_list.append(data_tuple)
+				for wheel_id in self.right_wheel_ids:
+					if data==0:
+						data_tuple = wheel_id, 0, 0
+						info_list.append(data_tuple)
+					else:	# Forward (255-128)
+						data = int(data / 2) # Round down
+						data_tuple = wheel_id, data, 0
+						info_list.append(data_tuple)
+						
+			# LJ for Left Wheels - # Example: <addr 2-7> <speed> <0>
+			elif name=='LJ/Right' or name=='LJ/Left':
+				info_list = None
+			elif name=='LJ/Up' or name=='LJ/Down':
+				for wheel_id in self.left_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 1, speed
+				for wheel_id in self.right_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-			for wheel_id in self.right_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data # makes forward # redundant, but human readable
-					data_tuple = wheel_id, 1, speed
-					info_list.append(data_tuple)
-		elif name=='RT':
-			# Left wheels Forward, Right wheels Reverse
-			for wheel_id in self.left_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data # makes forward # redundant, but human readable
-					data_tuple = wheel_id, 1, speed
-					info_list.append(data_tuple)
-			for wheel_id in self.right_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
-					info_list.append(data_tuple)
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 1, speed
-					info_list.append(data_tuple)
-					
-		# LJ for Left Wheels - # Example: <addr 2-7> <1> <speed>
-		elif name=='LJ/Right' or name=='LJ/Left':
-			info_list = None
-		elif name=='LJ/Up':
-			# data is fine unchanged because 0-100 are forward values
-			for wheel_id in self.left_wheel_ids:
-				data_tuple = wheel_id, 1, data
-				info_list.append(data_tuple)
 				
-		elif name=='LJ/Down':
-			# data is changed because 0 and 101-200 are reverse values
-			for wheel_id in self.left_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
+			# RJ for Right wheels - Example: <addr 2-7> <speed>
+			elif name=='RJ/Right' or name=='RJ/Left':
+				info_list = None
+			elif name=='RJ/Up' or name=='RJ/Down':
+				for wheel_id in self.left_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 1, speed
+				for wheel_id in self.right_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-			
-		# RJ for Right wheels - Example: <addr 2-7> <1> <speed>
-		elif name=='RJ/Right' or name=='RJ/Left':
-			info_list = None
-		elif name=='RJ/Up':
-			# data is fine unchanged because 0-100 are forward values
-			for wheel_id in self.right_wheel_ids:
-				data_tuple = wheel_id, 1, data
-				info_list.append(data_tuple)
-		elif name=='RJ/Down':
-			# data is changed because 0 and 101-200 are reverse values
-			for wheel_id in self.right_wheel_ids:
-				if data==0:
-					data_tuple = wheel_id, 1, 0
+		elif self.control_change==1: # Vector Steer Mode
+			if name=='LT':
+				info_list = None
+			elif name=='RT':
+				info_list = None
+				
+			# LJ for Drive Forward/Reverse - # Example: <addr 2-7> <speed> <0> for ALL 6 wheels at once
+			elif name=='LJ/Right' or name=='LJ/Left':
+				info_list = None
+			elif name=='LJ/Up' or name=='LJ/Down':
+				for wheel_id in self.left_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-					self.previous_LJ_Down = 0
-				else:
-					speed = data + 100 # +100 makes reverse
-					data_tuple = wheel_id, 1, speed
+				for wheel_id in self.right_wheel_ids:
+					data_tuple = wheel_id, data, 0
 					info_list.append(data_tuple)
-					self.previous_LJ_Down = speed
+
+			# RJ for Angle from 0-180 (0 starts at -90) - Example: <addr 2-7> <0> <angle>
+			# Calculate the x and y-components of this to compute the angle
+			elif name=='RJ/Right' or name=='RJ/Left':
+				info_list = None
+			elif name=='RJ/Up' or name=='RJ/Down':
+				info_list = None
+				
 		return info_list
-			
 	
 	def parse_pressed_button(self):
 		# Returns list in format ["button", 1(on)]
@@ -312,52 +296,51 @@ class JoyParser(object):
 			return None
 
 	def parse_pressed_joy(self):
-		# Returns list in format ["joy", amount 0.00-100.00]
+		# Returns list in format ["joy", amount 0-255]
 		if self.templist[7]=='\x02' and self.templist[6]=='\x02': # LT
 			if ord(self.templist[5])>=128:
-				val = "%.2f" % self.joy_254[ord(self.templist[5]) - 128]
+				val = str(ord(self.templist[5]) - 128)
 				return ["LT", val]
 			elif ord(self.templist[5])<=127:
-				val = "%.2f" % self.joy_254[ord(self.templist[5]) + 127]
+				val = str(ord(self.templist[5]) + 127)
 				return ["LT", val]
 		elif self.templist[7]=='\x05' and self.templist[6]=='\x02': # RT
 			if ord(self.templist[5])>=128:
-				val = "%.2f" % self.joy_254[ord(self.templist[5]) - 128]
+				val = str(ord(self.templist[5]) - 128)
 				return ["RT", val]
 			elif ord(self.templist[5])<=127:
-				val = "%.2f" % self.joy_254[ord(self.templist[5]) + 127]
+				val = str(ord(self.templist[5]) + 127)
 				return ["RT", val]
 		elif self.templist[7]=='\x00' and self.templist[6]=='\x02': # Left-Joy L/R
 			if ord(self.templist[5])<=127: # Right
-				val = "%.2f" % self.joy_127[ord(self.templist[5])]
+				val = str(ord(self.templist[5]))
 				return ["LJ/Right", val]
 			elif ord(self.templist[5])>=128: # Left
-				val = "%.2f" % self.joy_127[abs(ord(self.templist[5]) - 254)]
+				val = str(ord(self.templist[5]))
 				return ["LJ/Left", val]
 		elif self.templist[7]=='\x01' and self.templist[6]=='\x02': # Left-Joy U/D
 			if ord(self.templist[5])<=127: # Down
-				val = "%.2f" % self.joy_127[ord(self.templist[5])]
+				val = str(ord(self.templist[5]))
 				return ["LJ/Down", val]
 			elif ord(self.templist[5])>=128: # Up
-				val = "%.2f" % self.joy_127[abs(ord(self.templist[5]) - 254)]
+				val = str(ord(self.templist[5]) - 255)
 				return ["LJ/Up", val]
 		elif self.templist[7]=='\x03' and self.templist[6]=='\x02': # Right-Joy L/R
 			if ord(self.templist[5])<=127: # Right
-				val = "%.2f" % self.joy_127[ord(self.templist[5])]
+				val = str(ord(self.templist[5]))
 				return ["RJ/Right", val]
 			elif ord(self.templist[5])>=128: # Left
-				val = "%.2f" % self.joy_127[abs(ord(self.templist[5]) - 254)]
+				val = str(ord(self.templist[5]) - 255)
 				return ["RJ/Left", val]
 		elif self.templist[7]=='\x04' and self.templist[6]=='\x02': # Right-Joy U/D
 			if ord(self.templist[5])<=127: # Down
-				val = "%.2f" % self.joy_127[ord(self.templist[5])]
+				val = str(ord(self.templist[5]))
 				return ["RJ/Down", val]
 			elif ord(self.templist[5])>=128: # Up
-				val = "%.2f" % self.joy_127[abs(ord(self.templist[5]) - 254)]
+				val = str(ord(self.templist[5]) - 255)
 				return ["RJ/Up", val]
 		else:
 			return None
-
 
 	def parse_released(self):
 		# Returns list in format ["button/joy", 0(off)]
