9a6cbbcd011bb9584988f2f17056a01f95f98cc8
cbowie <bowie_cameron@yahoo.com>
2013-01-13 19:45:25 -0800

    Voltage and up/down wrist control added

--- GUI/arm_module.py
+++ GUI/arm_module.py
@@ -49,13 +49,13 @@ class Arm():
 		data = [(0,0),self.pos1,self.pos2,self.pos3]
 		lines = PolyLine(data)
 	
-		return PlotGraphics([lines],"Arm Display","Distance Out","Distance Up")
+		return PlotGraphics([lines],"Arm Simulation","Distance Out","Distance Up")
 	
 	
 class ArmSim(wx.Panel):
 
-	def __init__(self,parent):
-		wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY,size=(300,300))
+	def __init__(self,parent,size):
+		wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY,size=size)
 		#Creates the plot, sets arm angles, and plots arm
 		self.canvas = PlotCanvas(self)
 		self.arm = Arm(45,100,0)
@@ -72,7 +72,7 @@ class ArmSim(wx.Panel):
 
 class ArmControls(wx.Panel):
 	def __init__(self,parent,arm_sim):
-		wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY,size=(500,150))
+		wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY,size=(320,250))
 		
 		self.arm_sim = arm_sim
 		
@@ -87,56 +87,74 @@ class ArmControls(wx.Panel):
 		self.srv3 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.angle3))),size=(60, -1))
 		self.srv3.SetRange(0,360)
 		
-		btnIn = wx.Button(self,label=u"\u2190",size=(30,20))
-		btnUp = wx.Button(self,label=u"\u2191",size=(30,20))
-		btnOut = wx.Button(self,label=u"\u2192",size=(30,20))
-		btnDown = wx.Button(self,label=u"\u2193",size=(30,20))
+		btnIn = wx.Button(self,label=u"\u2190",size=(30,-1))
+		btnUp = wx.Button(self,label=u"\u2191",size=(30,-1))
+		btnOut = wx.Button(self,label=u"\u2192",size=(30,-1))
+		btnDown = wx.Button(self,label=u"\u2193",size=(30,-1))
 		
-		btnWristR = wx.Button(self,label=u"\u21BB")
-		btnWristL = wx.Button(self,label=u"\u21BA")
+		btnWristUp = wx.Button(self,label=u"\u2191",size=(30,-1))
+		btnWristR = wx.Button(self,label=u"\u21BB",size=(30,-1))
+		btnWristDown = wx.Button(self,label=u"\u2193",size=(30,-1))
+		btnWristL = wx.Button(self,label=u"\u21BA",size=(30,-1))
 		
-		btnScoopO = wx.Button(self,label='Open')
-		btnScoopC = wx.Button(self,label='Close')
+		btnScoopO = wx.Button(self,label='Open',size=(60,-1))
+		btnScoopC = wx.Button(self,label='Close',size=(60,-1))
+		
+		btnVoltUpdate = wx.Button(self,label='Update',size=(65,-1))
     	
 		btnIn.Bind(wx.EVT_BUTTON, lambda evt, temp='in': self.OnButton(evt, temp))
 		btnUp.Bind(wx.EVT_BUTTON, lambda evt, temp='up': self.OnButton(evt, temp))
 		btnOut.Bind(wx.EVT_BUTTON, lambda evt, temp='out': self.OnButton(evt, temp))
 		btnDown.Bind(wx.EVT_BUTTON, lambda evt, temp='down': self.OnButton(evt, temp))
+		btnWristUp.Bind(wx.EVT_BUTTON, lambda evt, temp='wrist up': self.OnButton(evt, temp))
 		btnWristR.Bind(wx.EVT_BUTTON, lambda evt, temp='wrist right': self.OnButton(evt, temp))
+		btnWristDown.Bind(wx.EVT_BUTTON, lambda evt, temp='wrist down': self.OnButton(evt, temp))
 		btnWristL.Bind(wx.EVT_BUTTON, lambda evt, temp='wrist left': self.OnButton(evt, temp))
 		btnScoopO.Bind(wx.EVT_BUTTON, lambda evt, temp='scoop open': self.OnButton(evt, temp))
 		btnScoopC.Bind(wx.EVT_BUTTON, lambda evt, temp='scoop close': self.OnButton(evt, temp))
+		btnVoltUpdate.Bind(wx.EVT_BUTTON, lambda evt, temp='voltage update': self.OnButton(evt, temp))
 		
 		armTxt = wx.StaticText(self,label='Arm')
 		wristTxt = wx.StaticText(self,label='Wrist')
 		scoopTxt = wx.StaticText(self,label='Scoop')
+		voltTxt = wx.StaticText(self,label='Voltage')
 		srv1Txt = wx.StaticText(self,label='Servo 1')
 		srv2Txt = wx.StaticText(self,label='Servo 2')
 		srv3Txt = wx.StaticText(self,label='Servo 3')
 		
+		voltOutput = wx.TextCtrl(self,-1,size=(60,-1))
+		
 		gridSizer = wx.GridBagSizer(3,3)
 		
-		gridSizer.Add(self.srv1,(1,1))
-		gridSizer.Add(self.srv2,(2,1))
-		gridSizer.Add(self.srv3,(3,1))
+		gridSizer.Add(self.srv1,(1,2))
+		gridSizer.Add(self.srv2,(2,2))
+		gridSizer.Add(self.srv3,(3,2))
 	
-		gridSizer.Add(btnIn,(2,2),flag=wx.EXPAND)
-		gridSizer.Add(btnUp,(1,3),flag=wx.EXPAND)
-		gridSizer.Add(btnOut,(2,4),flag=wx.EXPAND)
-		gridSizer.Add(btnDown,(3,3),flag=wx.EXPAND)
+		gridSizer.Add(btnIn,(2,3),flag=wx.EXPAND)
+		gridSizer.Add(btnUp,(1,4),flag=wx.EXPAND)
+		gridSizer.Add(btnOut,(2,5),flag=wx.EXPAND|wx.RIGHT,border=15)
+		gridSizer.Add(btnDown,(3,4),flag=wx.EXPAND)
 		
-		gridSizer.Add(armTxt,(0,2),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL|wx.TOP,border=14)
-		gridSizer.Add(wristTxt,(0,6),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL|wx.TOP,border=14)
-		gridSizer.Add(scoopTxt,(2,6),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL|wx.TOP,border=14)
+		gridSizer.Add(armTxt,(2,4),flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+		gridSizer.Add(wristTxt,(6,4),flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL)
+		gridSizer.Add(scoopTxt,(4,1),flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM)
+		gridSizer.Add(voltTxt,(6,1),flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM)
 		
-		gridSizer.Add(srv1Txt,(1,0),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
-		gridSizer.Add(srv2Txt,(2,0),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
-		gridSizer.Add(srv3Txt,(3,0),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+		gridSizer.Add(srv1Txt,(1,1),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+		gridSizer.Add(srv2Txt,(2,1),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+		gridSizer.Add(srv3Txt,(3,1),flag=wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
 		
-		gridSizer.Add(btnWristR,(1,7),flag=wx.EXPAND)
-		gridSizer.Add(btnWristL,(1,6),flag=wx.EXPAND)
-		gridSizer.Add(btnScoopO,(3,6),flag=wx.EXPAND)
-		gridSizer.Add(btnScoopC,(3,7),flag=wx.EXPAND)
+		gridSizer.Add(voltOutput,(7,2),flag=wx.FIXED_MINSIZE)
+		
+		gridSizer.Add(btnWristUp, (5, 4), flag=wx.EXPAND)
+		gridSizer.Add(btnWristR, (6, 5), flag=wx.EXPAND|wx.RIGHT,border=15)
+		gridSizer.Add(btnWristDown, (7, 4), flag=wx.EXPAND)
+		gridSizer.Add(btnWristL, (6, 3), flag=wx.EXPAND)
+		
+		gridSizer.Add(btnScoopO, (5, 1), flag=wx.FIXED_MINSIZE|wx.ALIGN_CENTER_HORIZONTAL)
+		gridSizer.Add(btnScoopC, (5, 2), flag=wx.FIXED_MINSIZE)
+		
+		gridSizer.Add(btnVoltUpdate, (7, 1), flag=wx.FIXED_MINSIZE|wx.ALIGN_CENTER_HORIZONTAL)
 		
 		gridSizer.AddGrowableCol(0)
 		gridSizer.AddGrowableCol(1)
