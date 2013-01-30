ac68aa2f887c6187e7e76023c39e39f1a957e310
Francis Vo <firefreakfrancis@gmail.com>
2013-01-29 16:02:45 -0800

    Integrated arm up/down/left/right movement into gui

--- GUI/arm_module.py
+++ GUI/arm_module.py
@@ -17,41 +17,64 @@ class Arm():
 	'''
 	This class calculates and stores arm angles and positions
 	'''
+	arget = [0,0]
+
+	seg = [{},{},{}]
 	#Arm segment lengths
-	LEN_ARM1 = 1.0
-	LEN_ARM2 = 1.0
-	LEN_ARM3 = 0.1
+	seg[0]['len'] = 1.4
+	seg[1]['len'] = 1.3
+	seg[2]['len'] = 1.0
 	
 	def __init__(self,angle1,angle2,angle3):
 		#This takes in the angle of each arm segment with the previous arm segment. Angle 1 is with the x-axis.
-		self.angle1 = math.radians(angle1)
-		self.angle2 = math.radians(angle2)
-		self.angle3 = math.radians(angle3)
+		
+		self.seg[0]['angle'] = math.radians(angle1)
+		self.seg[1]['angle'] = math.radians(angle2)
+		self.seg[2]['angle'] = math.radians(angle3)
 		
 		self.findPos()
+		target = [self.seg[1]['pos'][0],self.seg[1]['pos'][1]]
 		
+	def reach(self):
+		angle2 = self.seg[0]['angle']+self.seg[1]['angle']-math.pi
+		angle3 = angle2+self.seg[2]['angle']-math.pi
+
+		dx = self.target[0] - self.seg[0]['pos'][0]
+		dy = self.target[1] - self.seg[0]['pos'][1]
+		da = math.atan2(dy, dx) - angle2
+		angle2 += da/2
+		self.seg[1]['angle'] = angle2 - self.seg[0]['angle'] + math.pi
+
+		dx = self.target[0] - math.cos(angle2) * self.seg[1]['len']
+		dy = self.target[1] - math.sin(angle2) * self.seg[1]['len']
+		da = math.atan2(dy, dx) - self.seg[0]['angle']
+		self.seg[0]['angle'] += da
+		math.degrees(self.seg[0]['angle'])
+
+
+		self.seg[2]['angle'] = angle3 - angle2 + math.pi
+
 	def findPos(self):
 		#This calculates the second and third angle joints' angle with the x-axis
-		angle2 = self.angle1+self.angle2-math.pi
-		angle3 = angle2+self.angle3-math.pi
+		angle2 = self.seg[0]['angle']+self.seg[1]['angle']-math.pi
+		angle3 = angle2+self.seg[2]['angle']-math.pi
 		
 		#This determines the position of each joint based on the angle of each joint with the x-axis
-		self.pos1 = (self.LEN_ARM1*math.cos(self.angle1),
-		             self.LEN_ARM1*math.sin(self.angle1))
-		             
-		self.pos2 = (self.LEN_ARM2*math.cos(angle2)+self.pos1[0],
-		             self.LEN_ARM2*math.sin(angle2)+self.pos1[1])
-		             
-		self.pos3 = (self.LEN_ARM3*math.cos(angle3)+self.pos2[0],
-		             self.LEN_ARM3*math.sin(angle3)+self.pos2[1])
+		
+		self.seg[0]['pos'] = (self.seg[0]['len']*math.cos(self.seg[0]['angle']),self.seg[0]['len']*math.sin(self.seg[0]['angle']))
+		self.seg[1]['pos'] = (self.seg[1]['len']*math.cos(angle2)+self.seg[0]['pos'][0],self.seg[1]['len']*math.sin(angle2)+self.seg[0]['pos'][1])
+		self.seg[2]['pos'] = (self.seg[2]['len']*math.cos(angle3)+self.seg[1]['pos'][0],self.seg[2]['len']*math.sin(angle3)+self.seg[1]['pos'][1])
+		
 				
 	def drawArm(self):
-		data = [(0,0),self.pos1,self.pos2,self.pos3]
+		print self.seg[0]['angle'], self.seg[1]['angle'], self.seg[2]['angle']
+
+		self.findPos()
+		data = [(0,0),self.seg[0]['pos'],self.seg[1]['pos'],self.seg[2]['pos']]
 		lines = PolyLine(data)
 	
-		return PlotGraphics([lines],"Arm Simulation","Distance Out","Distance Up")
-	
-	
+		return PlotGraphics([lines],"Arm Display","Distance Out","Distance Up")
+
 class ArmSim(wx.Panel):
 
 	def __init__(self,parent,size):
@@ -59,7 +82,7 @@ class ArmSim(wx.Panel):
 		#Creates the plot, sets arm angles, and plots arm
 		self.canvas = PlotCanvas(self)
 		self.arm = Arm(45,100,0)
-		self.canvas.Draw(self.arm.drawArm(),xAxis=(0,2),yAxis=(0,2))
+		self.canvas.Draw(self.arm.drawArm(),xAxis=(-1,4),yAxis=(-1,4))
 
 		gridSizer = wx.GridBagSizer(3,3)
 		
@@ -78,13 +101,14 @@ class ArmControls(wx.Panel):
 		
 		self.Bind(wx.EVT_SPINCTRL, self.OnSpinCtrl)
 	
-		self.srv1 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.angle1))),size=(60, -1))
+		self.srv1 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.seg[0]['angle']))),size=(60, -1))
 		self.srv1.SetRange(0,90)
-		
-		self.srv2 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.angle2))),size=(60, -1))
+
+		self.srv2 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.seg[1]['angle']))),size=(60, -1))
 		self.srv2.SetRange(0,360)
 		
-		self.srv3 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.angle3))),size=(60, -1))
+
+		self.srv3 = wx.SpinCtrl(self,value='%d'%int(round(math.degrees(self.arm_sim.arm.seg[2]['angle']))),size=(60, -1))
 		self.srv3.SetRange(0,360)
 		
 		btnIn = wx.Button(self,label=u"\u2190",size=(30,-1))
@@ -168,12 +192,35 @@ class ArmControls(wx.Panel):
 		
 	def OnButton(self,e,value):
 		print(value)
+		self.arm_sim.arm.target = [self.arm_sim.arm.seg[1]['pos'][0],self.arm_sim.arm.seg[1]['pos'][1]]
+
+		if value == 'up':
+			print '^'
+			self.arm_sim.arm.target[1] += .01
+		elif value == 'down':
+			print 'v'
+			self.arm_sim.arm.target[1] -= .01
+		elif value == 'in':
+			print '<'
+			self.arm_sim.arm.target[0] -= .01
+		elif value == 'out':
+			print '>'
+			self.arm_sim.arm.target[0] += .01
+		for x in xrange(1,30):
+			self.arm_sim.arm.reach()
+
+		self.srv1.SetValue(math.degrees(self.arm_sim.arm.seg[0]['angle']))
+		self.srv2.SetValue(math.degrees(self.arm_sim.arm.seg[1]['angle']))
+		self.srv3.SetValue(math.degrees(self.arm_sim.arm.seg[2]['angle']))
+
+		self.arm_sim.canvas.Draw(self.arm_sim.arm.drawArm(),xAxis=(-1,4),yAxis=(-1,4))
+
 		
 	def OnSpinCtrl(self,e):
-		self.arm_sim.arm.angle1 = math.radians(self.srv1.GetValue())
-		self.arm_sim.arm.angle2 = math.radians(self.srv2.GetValue())
-		self.arm_sim.arm.angle3 = math.radians(self.srv3.GetValue())
-		self.arm_sim.arm.handAngle = self.arm_sim.arm.angle1+self.arm_sim.arm.angle2+self.arm_sim.arm.angle3
+		self.arm_sim.arm.seg[0]['angle'] = math.radians(self.srv1.GetValue())
+		self.arm_sim.arm.seg[1]['angle'] = math.radians(self.srv2.GetValue())
+		self.arm_sim.arm.seg[2]['angle'] = math.radians(self.srv3.GetValue())
+		self.arm_sim.arm.handAngle = self.arm_sim.arm.seg[0]['angle']+self.arm_sim.arm.seg[1]['angle']+self.arm_sim.arm.seg[2]['angle']
 		
 		self.arm_sim.arm.findPos()
-		self.arm_sim.canvas.Draw(self.arm_sim.arm.drawArm(),xAxis=(0,2),yAxis=(0,2))
+		self.arm_sim.canvas.Draw(self.arm_sim.arm.drawArm(),xAxis=(-1,4),yAxis=(-1,4))
