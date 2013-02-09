0f39b3d75a8aff9c99712c92204c0e1955b63027
Francis Vo <firefreakfrancis@gmail.com>
2013-02-09 15:37:15 -0800

    Vector Drive now working

--- GUI/rover_status.py
+++ GUI/rover_status.py
@@ -5,11 +5,12 @@
 # Description: A class to hold the rover variables. Designed to ease rover-gui interaction
 #############################
 
+import math
 
 class RoverStatus():
 
-    drive_mode = 'zeroRadius'
-    #drive_mode = 'vector'
+    # drive_mode = 'zeroRadius'
+    drive_mode = 'vector'
     # drive_mode = 'explicit'
 
     # 0    3
@@ -79,19 +80,19 @@ class RoverStatus():
     def SetAngle(self, angle):
         self.angle = math.radians(angle)
 
-        self.fl_angle = self.angle
-        self.fr_angle = self.angle
-        self.ml_angle = self.angle
-        self.mr_angle = self.angle
-        self.rl_angle = self.angle
-        self.rr_angle = self.angle
+        # self.fl_angle = self.angle
+        # self.fr_angle = self.angle
+        # self.ml_angle = self.angle
+        # self.mr_angle = self.angle
+        # self.rl_angle = self.angle
+        # self.rr_angle = self.angle
         
     def SetThrottle(self, throttle):
         self.throttle = throttle
         
-        self.fl_throttle = self.throttle
-        self.fr_throttle = self.throttle
-        self.ml_throttle = self.throttle
-        self.mr_throttle = self.throttle
-        self.rl_throttle = self.throttle
-        self.rr_throttle = self.throttle
+        # self.fl_throttle = self.throttle
+        # self.fr_throttle = self.throttle
+        # self.ml_throttle = self.throttle
+        # self.mr_throttle = self.throttle
+        # self.rl_throttle = self.throttle
+        # self.rr_throttle = self.throttle
