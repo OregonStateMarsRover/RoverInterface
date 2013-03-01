d5ccdf211b79afb1eec068a1be79bd5aa5873747
John Zeller <john@JohnUbuntu.(none)>
2013-03-01 10:48:56 -0800

    Testing can now be one off rover without adjusting the serial info in bus.py

--- Serial/bus.py
+++ Serial/bus.py
@@ -7,29 +7,44 @@
 # option to reset any or all ports by using the restart function.
 
 import serial
-#import time
-
 
 class Bus(object):
         def __init__(self):
+            try:
                 self.joy_rover = open('/dev/input/js0', 'r')
-# Disabled                self.joy_arm = open(port='/dev/input/js1', 'r')
-# Disabled                self.rover = serial.Serial(port='/dev/ttyUSB1',
-#                                        baudrate=115200)
-                # Perhaps auto detect which is from rover?
+            except:
+                print "Drive Joy is not found at /dev/input/js0"
+            try:
+                self.joy_arm = open('/dev/input/js1', 'r')
+            except:
+                print "Arm Joy is not found at /dev/input/js1"
+            try:
+                self.rover = serial.Serial('/dev/ttyUSB1',
+                                           baudrate=115200)
+            except:
+                print "Rover is not found at /dev/ttyUSB1"
 
         def restart(self, bus_name):
                 if bus_name == 'joy_rover':
-                        self.joy_rover.close()
-                        self.joy_rover = open('/dev/input/js1', 'r')
-# Disabled                elif bus_name=='joy_arm':
-#                        self.joy_arm.close()
-#                        self.joy_arm = open('/dev/input/js0', 'r')
+                    self.joy_rover.close()
+                    try:
+                        self.joy_rover = open('/dev/input/js0', 'r')
+                    except:
+                        print "Drive Joy is not found at /dev/input/js0"
+                elif bus_name=='joy_arm':
+                    self.joy_arm.close()
+                    try:
+                        self.joy_arm = open('/dev/input/js1', 'r')
+                    except:
+                        print "Arm Joy is not found at /dev/input/js1"
                 elif bus_name == 'rover':
-                        self.tripod.close()
-                        self.tripod = serial.Serial(port='/dev/ttyUSB1',
-                                                    baudrate=115200)
+                    self.tripod.close()
+                    try:
+                        self.rover = serial.Serial('/dev/ttyUSB1',
+                                                   baudrate=115200)
+                    except:
+                        print "Rover is not found at /dev/ttyUSB1"
                 elif bus_name == 'all':
                         self.restart('joy_rover')
-# Disabled                        self.restart('joy_arm')
+                        self.restart('joy_arm')
                         self.restart('rover')
