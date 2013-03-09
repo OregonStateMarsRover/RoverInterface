cbf8e7867732e4aee48ca66df79fd89871260398
Francis Vo <firefreakfrancis@gmail.com>
2013-03-09 13:09:37 -0800

    got controller controlling arm

---
+++ GUI/modules/arm_math.py
@@ -0,0 +1,28 @@
+import math
+
+
+def updateArm(self):
+    target = [self.roverStatus.arm_seg[1]['pos'][0], self.roverStatus.arm_seg[1]['pos'][1]]
+
+    target[1] += .001 * self.roverStatus.arm_joy_states['RJ/UpDown']
+    target[0] += .001 * self.roverStatus.arm_joy_states['RJ/LeftRight']
+    reach(self.roverStatus, target)
+
+
+def reach(roverStatus, target):
+    angle2 = roverStatus.arm_seg[0]['angle'] + roverStatus.arm_seg[1]['angle'] - math.pi
+    angle3 = angle2 + roverStatus.arm_seg[2]['angle'] - math.pi
+
+    dx = target[0] - roverStatus.arm_seg[0]['pos'][0]
+    dy = target[1] - roverStatus.arm_seg[0]['pos'][1]
+    da = math.atan2(dy, dx) - angle2
+    angle2 += da / 2
+    roverStatus.arm_seg[1]['angle'] = angle2 - roverStatus.arm_seg[0]['angle'] + math.pi
+
+    dx = target[0] - math.cos(angle2) * roverStatus.arm_seg[1]['len']
+    dy = target[1] - math.sin(angle2) * roverStatus.arm_seg[1]['len']
+    da = math.atan2(dy, dx) - roverStatus.arm_seg[0]['angle']
+    roverStatus.arm_seg[0]['angle'] += da
+    math.degrees(roverStatus.arm_seg[0]['angle'])
+
+    roverStatus.arm_seg[2]['angle'] = angle3 - angle2 + math.pi
