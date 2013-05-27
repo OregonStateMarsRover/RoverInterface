963b7df53c15d07eca493ab82f3c3178f2b8f522
rover <rover@marsrover.(none)>
2013-05-27 15:45:19 -0700

    Disabled launch of GPS module

--- go.sh
+++ go.sh
@@ -1,7 +1,5 @@
 echo "Removing all .pyc files";
 rm -r ./*.pyc
 echo "Starting GUI"
-python GUI/gui.py &
-echo "Starting GPS Module"
-python GUI/modules/gps_module.py &
+python GUI/gui.py
 echo "GUI Started, launching Receptionist"
